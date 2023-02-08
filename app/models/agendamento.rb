980bfb9afb6219ee2c9550f808b1b2d50790c55d
wironjr <jun1n_@live.com>
2023-02-07 21:44:31 -0300

    ajusta array de serviços no select na action create

--- app/models/agendamento.rb
+++ app/models/agendamento.rb
@@ -1,7 +1,6 @@
 class Agendamento < ApplicationRecord
 	validates :nome, presence: true
 	validates :hora, presence: true
-	validates :data, presence: true
 	validates :servico, presence: true
 	validates :telefone, presence: true
 end
