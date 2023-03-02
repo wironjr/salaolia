cdf9a8bf9d6d39a904966062125563c2207cb004
wironjr <jun1n_@live.com>
2023-03-02 00:07:17 -0300

    ajustes nas validaçoes de usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 #reforça a segurança e cria o digest
     has_secure_password
 
-    validates :nome, presence: true, length: {minimum:3, maximum:50}, uniqueness: { case_sensitive: false } 
+    validates :nome, presence: true, length: {minimum:3, maximum:50}, format: { without: /\s/ }, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: {minimum: 6}
 
     has_many :agendamentos
