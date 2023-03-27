0991fab00c87460eabbf3d307fbdddbd3bf39d0d
wironjr <jun1n_@live.com>
2023-03-27 14:36:36 -0300

    ajusta uppercase no tela sessions new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(nome: params[:session][:nome])
+    user = User.find_by(nome: params[:session][:nome].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in(user)
       redirect_to root_path
