31c0b482e28c9d15de6dd8830f7f28c62678cff8
wironjr <jun1n_@live.com>
2023-03-27 14:30:39 -0300

    ajusta os horarios de agendamento

--- app/controllers/agendamentos_controller.rb
+++ app/controllers/agendamentos_controller.rb
@@ -144,7 +144,7 @@ class AgendamentosController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_agendamento
-      @agendamento = current_user.agendamentos.find(params[:id])
+      @agendamento = Agendamento.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
